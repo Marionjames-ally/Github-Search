7dd5bdc7480520e1c33a48d5e2565338e9ba528a
marionjames <maarionjames2018@gmail.com>
2019-11-02 18:43:08 +0300

    make API request using promise and console the results

--- src/app/services/github-http-service.service.ts
+++ src/app/services/github-http-service.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubHttpServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 searchGithub(searchTerm:string){
-  console.log(searchTerm);
+  let searchEndpoint="https://api.github.com/search?api_key="+environment.GITHUBAPIKEY;
+  searchEndpoint += "&q"+ searchTerm;
+  let promise = new Promise((resolve, reject)=>{
+    this.http.get(searchEndpoint).toPromise().then(
+      (results)=>{
+        console.log(results);
+        resolve()
+      },
+      (error)=>{
+        console.log(error)
+        reject()
+      }
+    )
+  })
+  return promise;
 }
 
 }
