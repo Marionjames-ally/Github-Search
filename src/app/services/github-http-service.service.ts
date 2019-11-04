9e5ef049b91674c235bd8de951f8f8c239ea1d13
marionjames <maarionjames2018@gmail.com>
2019-11-04 08:08:55 +0300

    create an array and store the response in it

--- src/app/services/github-http-service.service.ts
+++ src/app/services/github-http-service.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Github } from '../github';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubHttpServiceService {
-
+  github: any[];
   constructor(private http:HttpClient) { }
 searchGithub(searchTerm:string){
   let searchEndpoint="https://api.github.com/search?api_key="+environment.GITHUBAPIKEY;
@@ -14,7 +15,13 @@ searchGithub(searchTerm:string){
   let promise = new Promise((resolve, reject)=>{
     this.http.get(searchEndpoint).toPromise().then(
       (results)=>{
-        console.log(results);
+       this.github=[];
+       for(let i=0; i<results["data"].length; i++){
+        let url = results["data"][i]["repositories"]["fixed_height"]["url"];
+        let giph = new Github(url);
+        this.github.push(Github);
+      }
+        console.log(this.github);
         resolve()
       },
       (error)=>{
